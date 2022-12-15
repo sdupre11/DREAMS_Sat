126e191b43e46cd57dafb7a34e703ed19e739265
Sam Dupre <sdupre@rstudio-server.testing.ap.datim.org>
2022-12-15 12:06:50 +0000

    prepping for downloadHandler

---
+++ app/data_load.R
@@ -0,0 +1,36 @@
+
+
+countryData <- readRDS("data/countryData.RDS")
+
+AGYW_PREV <- s3read_using(FUN = read.csv,
+             bucket = Sys.getenv("TEST_BUCKET_WRITE"),
+             object = "system_dreams_saturation/AGYW_PREVbyCountry.csv") %>%
+  as.data.frame()
+
+countryDataJoined <- left_join(countryData,
+                               AGYW_PREV,
+                               by = c("AREA_NAME" = "AREA_NAME", "fiscal_year" = "fiscal_year", "country" = "country", "ageasentered" = "ageasentered")) %>%
+  select(-c("snu1",
+            "X"))
+
+botADM1.sf <- readRDS('data/BotswanaADM1.RDS')
+botADM2.sf <- readRDS('data/BotswanaADM2.RDS')
+kenADM1.sf <- readRDS('data/KenyaADM1.RDS')
+kenADM2.sf <- readRDS('data/KenyaADM2.RDS')
+kenADM1.sf <- readRDS('data/LesothoADM1.RDS')
+zimADM1.sf <- readRDS('data/ZimbabweADM1.RDS')
+zimADM2.sf <- readRDS('data/ZimbabweADM2.RDS')
+
+Ages <- c(10:24) 
+`2018` <- rep(20,15)
+`2019` <- rep(20,15)
+`2020` <- rep(20,15)
+`2021` <- rep(20,15)
+`2022` <- rep(20,15)
+
+structure_table <- data.frame(Ages,
+                              `2018`,
+                              `2019`,
+                              `2020`,
+                              `2021`,
+                              `2022`)
